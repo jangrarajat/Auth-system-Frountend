16b76b43daa4c4e1f3f33a433705e952b60b3328
jangrarajat <rajatjangra940@gmail.com>
2025-08-24 21:23:57 +0530

    updating login and signup lfile

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
-import ErrorMessage from '../components/errorMessage'
+import ErrorMessage from '../components/ErrorMessage'
 import axios from 'axios'
 import SuccessMessage from '../components/SuccessMessage'
 import Laoder from '../components/Laoder'
