441c49ff1ef89ba2d2d4e400d32b97421182c700
jangrarajat <rajatjangra940@gmail.com>
2025-08-24 22:14:25 +0530

    updating navebar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -79,13 +79,13 @@ function Home() {
         
 
       <nav className='border border-b-gray-700 mb-4 w-full h-20 flex flex-row items-center justify-between px-3'>
-        <div><h1 className='font-extrabold'>MY <span className='text-blue-600 font-extrabold'>App</span></h1></div>
+        <div><h1 className='font-extrabold'>Welcome<span className='text-blue-600 font-extrabold ml-1'>{loggedInUser}</span></h1></div>
 
         <div>
 
           <div className='flex flex-row items-center gap-3'>
             <div className=' w-16 flex flex-row  justify-center items-center  cursor-pointer '>
-               <h1 className=' font-thin ' >{loggedInUser} </h1>
+           
               <img className='w-full' src="https://i.pinimg.com/1200x/d2/25/07/d2250772dc3221bfe9ed14d1d4cf0ec7.jpg" alt="user logo" />
             </div>
 
